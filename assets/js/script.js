f5903653f4adc8f4bf5acfb989a430c624038ec2
Michael Kendrick <michael@locumsmart.net>
2022-02-28 16:32:20 -0600

    drag and drop completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -143,3 +143,44 @@ $(".list-group").on("blur", "input[type='text']", function () {
   // replace input with span element
   $(this).replaceWith(taskSpan);
 });
+
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function (event) {},
+  deactivate: function (event) {},
+  over: function (event) {},
+  out: function (event) {},
+  update: function (event) {
+    var tempArr = [];
+    $(this)
+      .children()
+      .each(function () {
+        var text = $(this).find("p").text().trim();
+        var date = $(this).find("span").text().trim();
+        tempArr.push({
+          text: text,
+          date: date,
+        });
+      });
+    var arrName = $(this).attr("id").replace("list-", "");
+    tasks[arrName] = tempArr;
+    saveTasks();
+    console.log(tempArr);
+  },
+});
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function (event, ui) {
+    ui.draggable.remove();
+  },
+  over: function (event, ui) {
+    console.log("over");
+  },
+  out: function (event, ui) {
+    console.log("out");
+  },
+});
